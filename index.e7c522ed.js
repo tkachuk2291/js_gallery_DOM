d0180815ce78eef115fee1b4f0a19a41e572bd55
Anton Tkachuk <tkacuk2291@gmail.com>
2025-01-02 12:38:32 +0200

    make build

---
+++ index.e7c522ed.js
@@ -0,0 +1,2 @@
+document.getElementById("thumbs").addEventListener("click",function(e){event.target.closest("a")&&(event.preventDefault(),document.getElementById("largeImg").src=e.target.closest("a").href)});
+//# sourceMappingURL=index.e7c522ed.js.map
